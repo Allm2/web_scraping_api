00b62206965b735d6f2494edc63e5eafb73dd319
Antonio Raggid <antonioraggid@gmail.com>
2022-03-09 14:51:10 -0400

    Paginacao

--- web_scraping/service/scraping_service.py
+++ web_scraping/service/scraping_service.py
@@ -9,30 +9,41 @@ def colhendo_dados():
     # especifique o URL
     url = "https://am.olx.com.br/regiao-de-manaus/manaus/cidade-nova/imoveis/venda/apartamentos"
 
+    soup = get_soup(url)
+    all_ads_data = []
+
+    while True:
+        # Busque pela tag <ul> que contem a lista de anuncios
+        ads_list = soup.find('ul', {'id': 'ad-list'}).findAll('a')
+
+        for ad in ads_list:
+            all_ads_data.append(colhendo_dados_anuncio(ad['href']))
+
+        next_page = soup.find('a', {'data-lurker-detail': 'next_page'})
+
+        if next_page:
+            soup = get_soup(next_page['href'])
+
+        else:
+            break
+
+    # collection_name = get_connection()['list_announcement']
+    #
+    # collection_name.insert_many(all_ads_data)
+    print(len(all_ads_data))
+
+
+def get_soup(url):
     # Consulte o site e retorne o html para a variável 'page'
     request = urllib.request.Request(url, headers={'user-agent': 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'})
     page = urllib.request.urlopen(request)
+    # Parse o html na variável 'page' e devolva-o no formato BeautifulSoup
+    return BeautifulSoup(page, 'html.parser')
 
-    # Parse o html na variável 'page' e armazene-o no formato BeautifulSoup
-    soup = BeautifulSoup(page, 'html.parser')
-
-    # Busque pela tag <ul> que contem a lista de anuncios
-    ads_list = soup.find('ul', {'id': 'ad-list'}).findAll('a')
-
-    all_ads_data = []
-    for ad in ads_list:
-        all_ads_data.append(colhendo_dados_anuncio(ad['href']))
-
-    collection_name = get_connection()['list_announcement']
-
-    collection_name.insert_many(all_ads_data)
 
 
 def colhendo_dados_anuncio(url):
-    request = urllib.request.Request(url, headers={'user-agent': 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'})
-    page = urllib.request.urlopen(request)
-    soup = BeautifulSoup(page, "html.parser")
-
+    soup = get_soup(url)
     json_data = json.loads(soup.find('script', {'id': 'initial-data'})['data-json'])['ad']
 
     final_ad = {
